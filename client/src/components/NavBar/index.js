1d2dead45665f87c663b3873546c93cafeb4e75b
edemskiy <qapple@ya.ru>
2019-12-30 23:42:50 +0300

    Change NavBar markup

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,26 +1,30 @@
-import React from "react";
-import "./NavBar.css";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./NavBar.scss";
 
-export function NavBar(props) {
+export function NavBar() {
+  const { logout } = useContext(AuthContext);
   return (
     <>
       <div className="navbar-wrapper">
         <nav>
           <div className="menu-and-logo">
             <div className="menu-icon">
-              <i class="fas fa-bars"></i>
+              <i className="fas fa-bars"></i>
             </div>
-            <a className="logo">
+            <a href="#" className="logo">
               <i className="far fa-sticky-note"></i>
               <span>Notes</span>
             </a>
           </div>
           <div className="search-and-tools">
-            <div className="searchbar">
-              <input placeholder="search" />
+            <div className="search-bar">
+              <form className="search-form">
+                <input placeholder="search" />
+              </form>
             </div>
             <div className="tools">
-              <button className="btn-logout" onClick={props.logout}>
+              <button className="btn-logout" onClick={logout}>
                 Logout
               </button>
             </div>
