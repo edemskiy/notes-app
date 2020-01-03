0335865a5abaf8545975c9e1fbeb6d325d786576
edemskiy <qapple@ya.ru>
2020-01-03 15:07:12 +0300

    Fix auth router import

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", require("./routes/auth.route"));
 
 mongoose
   .connect(process.env.MONGODB_URI, {
