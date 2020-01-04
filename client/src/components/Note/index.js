f01c6e25aa533a410d50491ab5615548f2a19eed
edemskiy <qapple@ya.ru>
2020-01-04 17:06:05 +0300

    Add tools to note component

--- client/src/components/Note/index.js
+++ client/src/components/Note/index.js
@@ -4,12 +4,23 @@ import "./Note.scss";
 
 export function Note({ note }) {
   return (
-    <div className="note">
-      <div className="title">{note.title}</div>
-      <div className="text">
+    <div className="note-card">
+      <div className="note-title">{note.title}</div>
+      <div className="note-text">
         {note.text.split("\n").map((line, i) => (
           <div key={i}>{line}</div>
         ))}
+      </div>
+      <div className="pin-btn">
+        <i className="fas fa-thumbtack"></i>
+      </div>
+      <div className="note-tools">
+        <div className="color-picker">
+          <i className="fas fa-palette"></i>
+        </div>
+        <div className="delete-btn">
+          <i className="fas fa-trash"></i>
+        </div>
       </div>
     </div>
   );
