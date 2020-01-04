f680c90d8b389f4d095a3a602cf366b843ad8540
edemskiy <qapple@ya.ru>
2020-01-04 18:22:44 +0300

    Add note color picker

--- client/src/components/Note/index.js
+++ client/src/components/Note/index.js
@@ -2,21 +2,39 @@ import React from "react";
 
 import "./Note.scss";
 
+const colors = {
+  white: "white",
+  red: "red",
+  blue: "blue",
+  green: "green",
+  purple: "purple",
+  pink: "pink",
+  yellow: "yellow",
+  grey: "grey"
+};
+
 export function Note({ note }) {
   return (
     <div className="note-card">
       <div className="note-title">{note.title}</div>
+
       <div className="note-text">
         {note.text.split("\n").map((line, i) => (
           <div key={i}>{line}</div>
         ))}
       </div>
+
       <div className="pin-btn">
         <i className="fas fa-thumbtack"></i>
       </div>
       <div className="note-tools">
         <div className="color-picker">
           <i className="fas fa-palette"></i>
+        </div>
+        <div className="color-palette">
+          {Object.keys(colors).map((color, i) => (
+            <div key={i} style={{ backgroundColor: colors[color] }}></div>
+          ))}
         </div>
         <div className="delete-btn">
           <i className="fas fa-trash"></i>
