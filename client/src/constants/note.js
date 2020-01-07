4ed42a7ea622951d5a96c0e6096e0e311c4b30af
edemskiy <qapple@ya.ru>
2020-01-07 22:51:14 +0300

    Export note colors to constant

---
+++ client/src/constants/note.js
@@ -0,0 +1,10 @@
+export const noteColors = {
+  white: "#fff",
+  red: "#f28b82",
+  blue: "#aecbfa",
+  green: "#ccff90",
+  purple: "#d7aefb",
+  pink: "#fdcfe8",
+  yellow: "#fff475",
+  grey: "#e8eaed"
+};
