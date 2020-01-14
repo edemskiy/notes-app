a93d6aba711b7a2568ffd38cdd8486e1834a493f
edemskiy <qapple@ya.ru>
2020-01-14 14:43:13 +0300

    Add prod and dev functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 const app = express();
@@ -9,6 +10,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/notes", require("./routes/notes.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(process.env.MONGODB_URI, {
